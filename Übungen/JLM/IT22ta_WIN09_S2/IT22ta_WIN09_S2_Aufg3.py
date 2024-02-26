d9e2104b17df1a49157c4e0b3e3d7586c5599f3a
leiselau <leiselau@students.zhaw.ch>
2024-02-26 18:26:56 +0100

    Aufgabe 3 auskommentiert

--- Übungen/JLM/IT22ta_WIN09_S2/IT22ta_WIN09_S2_Aufg3.py
+++ Übungen/JLM/IT22ta_WIN09_S2/IT22ta_WIN09_S2_Aufg3.py
@@ -1,20 +1,20 @@
-import sympy as sp
+# import sympy as sp
 
-x0 = sp.Matrix([1.5,3,2.5])
-x1, x2, x3 = sp.symbols('x1 x2 x3')
-x = sp.Matrix([x1,x2,x3])
+# x0 = sp.Matrix([1.5,3,2.5])
+# x1, x2, x3 = sp.symbols('x1 x2 x3')
+# x = sp.Matrix([x1,x2,x3])
 
-f1 = x1 + x2**2 - x3**2 -13
-f2 = sp.log(x2/4) + sp.exp(0.5*x3-1) - 1
-f3 = (x2 - 3)**2 - x3**3 + 7
+# f1 = x1 + x2**2 - x3**2 -13
+# f2 = sp.log(x2/4) + sp.exp(0.5*x3-1) - 1
+# f3 = (x2 - 3)**2 - x3**3 + 7
 
-f = sp.Matrix([f1,f2,f3])
-Df = f.jacobian(x)
+# f = sp.Matrix([f1,f2,f3])
+# Df = f.jacobian(x)
 
-subs = [(x1,1.5), (x2,3), (x3,2.5)]
+# subs = [(x1,1.5), (x2,3), (x3,2.5)]
 
-Df0 =  Df.subs(subs)
-f0 = f.subs(subs)
+# Df0 =  Df.subs(subs)
+# f0 = f.subs(subs)
 
-g = f0 + Df0 * (x-x0)
-print(g)
+# g = f0 + Df0 * (x-x0)
+# print(g)
