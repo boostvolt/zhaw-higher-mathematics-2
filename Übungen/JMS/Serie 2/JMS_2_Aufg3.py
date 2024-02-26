46d9981c991011b896e9b0129022e72bc3e00a8c
Minh Khanh Nguyen <nguyemin@students.zhaw.ch>
2024-02-26 17:41:04 +0100

    add aufgabe 3 serie 2

--- Übungen/JMS/Serie 2/JMS_2_Aufg3.py
+++ Übungen/JMS/Serie 2/JMS_2_Aufg3.py
@@ -1,7 +1,17 @@
 import sympy as sp
 
-x1, x2, x3 = sp.symbols("x1 x2 x3")
+x1, x2, x3 = sp.symbols("x1 x2 x3", positive=True)
 
 f1 = x1 + x2**2 - x3**2 - 13
-f2 = sp.ln(x2/4) + sp.exp(0.5*x3-1) - 1
-f3 = (x2 - 3) ** 2 - x3 ** 3 + 7
+f2 = sp.log(x2 / 4) + sp.exp(0.5 * x3 - 1) - 1
+f3 = (x2 - 3) ** 2 - x3**3 + 7
+
+F = sp.Matrix([f1, f2, f3])
+x = sp.Matrix([x1, x2, x3])
+x0 = sp.Matrix([1.5, 3, 2.5])
+
+g = F.subs([(x1, 1.5), (x2, 3), (x3, 2.5)]) + F.jacobian([x1, x2, x3]).subs(
+    [(x1, 1.5), (x2, 3), (x3, 2.5)]
+) * (x - x0)
+
+print(g)
