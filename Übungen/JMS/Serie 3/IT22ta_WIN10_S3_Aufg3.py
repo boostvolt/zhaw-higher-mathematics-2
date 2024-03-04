a854f28b04c2d0cb4c189d218fdc95790988c326
Minh Khanh Nguyen <nguyemin@students.zhaw.ch>
2024-03-04 17:46:15 +0100

    last stand

--- Übungen/JMS/Serie 3/IT22ta_WIN10_S3_Aufg3.py
+++ Übungen/JMS/Serie 3/IT22ta_WIN10_S3_Aufg3.py
@@ -1,25 +1,36 @@
-from sympy import Matrix, symbols, sympify, plot_implicit, Eq
+from sympy import Matrix, symbols, sympify
 
-def get_minimum_k(functions, initial_guess):
-    variables = symbols(' '.join(initial_guess.keys()))
-    jacobian_matrix = Matrix(functions).jacobian(variables)
-    delta = -jacobian_matrix.inv().subs(initial_guess) * Matrix(functions).subs(initial_guess)
-    for i in range(10):
-        if(initial_guess + delta)
 
 def newton_method(functions, initial_guess, tolerance, debug=False):
     functions = [sympify(f) for f in functions]  # Convert strings to SymPy expressions
-    variables = symbols(' '.join(initial_guess.keys()))
-    jacobian_matrix = Matrix(functions).jacobian(variables)
+    variables = symbols(" ".join(initial_guess.keys()))
     current_guess = initial_guess.copy()
 
-
-
     while True:
-        delta = -jacobian_matrix.inv().subs(current_guess) * Matrix(functions).subs(current_guess)
+        jacobian_matrix = Matrix(functions).jacobian(variables)
+        delta = -jacobian_matrix.inv().subs(current_guess) * Matrix(functions).subs(
+            current_guess
+        )
+        k = 0
+        condition_guess = current_guess.copy()
+        for i in range(10, -1, -1):
+            for j, var in enumerate(variables):
+                condition_guess[str(var)] = (
+                    current_guess[str(var)] + (delta[j] / 2**i)
+                ).evalf()
+            f_norm_condition = Matrix(functions).subs(condition_guess).norm()
+
+            print(f_norm_condition.evalf())
+            print(Matrix(functions).subs(condition_guess).evalf())
+
+            if f_norm_condition > Matrix(functions).subs(current_guess).norm():
+                k = i + 1
+                break
 
         for j, var in enumerate(variables):
-            current_guess[str(var)] = (current_guess[str(var)] + delta[j]).evalf() * 0.5
+            current_guess[str(var)] = (
+                current_guess[str(var)] + (delta[j] / 2**k)
+            ).evalf()
 
         f_norm = Matrix(functions).subs(current_guess).norm()
 
@@ -36,5 +47,8 @@ def newton_method(functions, initial_guess, tolerance, debug=False):
 functions = [
     "x1 + x2 - x3**2 - 13",
     "log(x2/4) + exp(0.5 * x3) - 1",
-    "(x2 - 3)**2 - x3**3 + 7"
+    "(x2 - 3)**2 - x3**3 + 7",
 ]
+
+x_0 = {"x1": 1.5, "x2": 3, "x3": 2.5}
+newton_method(functions, x_0, 10**-5, True)
