f63f7063409a41b06c615490d2744f6eec5d0d47
Jan Kott <51777660+boostvolt@users.noreply.github.com>
2024-03-10 17:14:30 +0100

    feat: use correct functions in task 3

--- Übungen/JMS/Serie 3/IT22ta_WIN10_S3_Aufg3.py
+++ Übungen/JMS/Serie 3/IT22ta_WIN10_S3_Aufg3.py
@@ -1,6 +1,5 @@
 from sympy import Matrix, symbols, sympify
 
-
 def newton_method(functions, initial_guess, tolerance, debug=False):
     functions = [sympify(f) for f in functions]  # Convert strings to SymPy expressions
     variables = symbols(" ".join(initial_guess.keys()))
@@ -8,29 +7,20 @@ def newton_method(functions, initial_guess, tolerance, debug=False):
 
     while True:
         jacobian_matrix = Matrix(functions).jacobian(variables)
-        delta = -jacobian_matrix.inv().subs(current_guess) * Matrix(functions).subs(
-            current_guess
-        )
+        delta = -jacobian_matrix.inv().subs(current_guess) * Matrix(functions).subs(current_guess)
         k = 0
         condition_guess = current_guess.copy()
         for i in range(10, -1, -1):
             for j, var in enumerate(variables):
-                condition_guess[str(var)] = (
-                    current_guess[str(var)] + (delta[j] / 2**i)
-                ).evalf()
+                condition_guess[str(var)] = (current_guess[str(var)] + (delta[j] / 2**i)).evalf()
             f_norm_condition = Matrix(functions).subs(condition_guess).norm()
-
-            print(f_norm_condition.evalf())
-            print(Matrix(functions).subs(condition_guess).evalf())
 
             if f_norm_condition > Matrix(functions).subs(current_guess).norm():
                 k = i + 1
                 break
 
         for j, var in enumerate(variables):
-            current_guess[str(var)] = (
-                current_guess[str(var)] + (delta[j] / 2**k)
-            ).evalf()
+            current_guess[str(var)] = (current_guess[str(var)] + (delta[j] / 2**k)).evalf()
 
         f_norm = Matrix(functions).subs(current_guess).norm()
 
@@ -40,12 +30,10 @@ def newton_method(functions, initial_guess, tolerance, debug=False):
         if f_norm < tolerance:
             break
 
-    print()
     return current_guess
 
-
 functions = [
-    "x1 + x2 - x3**2 - 13",
+    "x1 + x2**2 - x3**2 - 13",
     "log(x2/4) + exp(0.5 * x3) - 1",
     "(x2 - 3)**2 - x3**3 + 7",
 ]
