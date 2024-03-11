3995dea52a32c267fbff93c827580db1d0880ea1
leiselau <leiselau@students.zhaw.ch>
2024-03-11 14:59:54 +0100

    Not i l

--- Übungen/JLM/IT22ta_WIN09_S4/IT22ta_WIN09_S4_Aufg2.py
+++ Übungen/JLM/IT22ta_WIN09_S4/IT22ta_WIN09_S4_Aufg2.py
@@ -1,17 +1,16 @@
 import numpy as np
 
-
 # Lagrange-Interpolation
 def lagrange_int(x, y, x_int):
     p = 0
     n = len(x)
 
     for i in range(0, n):
-        i_n = 1
+        l_n = 1
         for j in range(0, n):
             if j != i:
-                i_n *= (x_int - x[j]) / (x[i] - x[j])
-        p += y[i] * i_n
+                l_n *= (x_int - x[j]) / (x[i] - x[j])
+        p += y[i] * l_n
     return p
 
 # Gegebene Werte
@@ -23,10 +22,10 @@ y_int = lagrange_int(x, y, x_int)
 # Ergebnis
 print(y_int)
 
-
-""" import numpy as np
+"""
 from scipy.interpolate import lagrange
 x = np.array([0,2500,5000,10000])
 y = np.array([1013,747,540,226])
 poly = lagrange(x, y)
-print(poly(3750)) """
+print(poly(3750)) 
+"""
