779b7257297043462c1a173a92a5c50720ff42ca
kappmar1 <kappmar1@students.zhaw.ch>
2024-03-11 14:57:22 +0100

    "S4 refactored"

--- Übungen/JLM/IT22ta_WIN09_S4/IT22ta_WIN09_S4_Aufg2.py
+++ Übungen/JLM/IT22ta_WIN09_S4/IT22ta_WIN09_S4_Aufg2.py
@@ -1,25 +1,26 @@
 import numpy as np
 
 
+# Lagrange-Interpolation
 def lagrange_int(x, y, x_int):
     p = 0
-    i = 0
-    for i in range(np.size(x)):
-        j = 0
-        l = 1
-        for j in range(np.size(x)):
-            if i != j:
-                l *= (x_int - x[j]) / (x[i] - x[j])
-        p += l * y[i]
+    n = len(x)
 
+    for i in range(0, n):
+        i_n = 1
+        for j in range(0, n):
+            if j != i:
+                i_n *= (x_int - x[j]) / (x[i] - x[j])
+        p += y[i] * i_n
     return p
 
-
+# Gegebene Werte
 x = np.array([0, 2500, 5000, 10000])
 y = np.array([1013, 747, 540, 226])
 x_int = 3750
 y_int = lagrange_int(x, y, x_int)
 
+# Ergebnis
 print(y_int)
 
 
