c48ad511ec51ccb19705605aa84b40a816a0498e
Jan Kott <51777660+boostvolt@users.noreply.github.com>
2024-03-18 16:03:07 +0100

    refactor: remove test file

--- Übungen/JMS/Serie 5/IT22ta_WIN10_S5_Aufg2.py
+++ Übungen/JMS/Serie 5/IT22ta_WIN10_S5_Aufg2.py
@@ -1,32 +1,32 @@
-from ctypes.wintypes import HACCEL
 import numpy as np
 
 x = np.array([4, 6, 8, 10])
 y = np.array([6, 3, 9, 0])
-xx = np.array([])
-
-print(y[:-1])
+xx = np.arange(4, 10, 0.1)
 
 def spline(x, y, xx):
     if (x.size != y.size):
         return "x and y must have the same size"
-    a = np.zeros_like(y)
-    h = np.zeros_like(len(x) - 1)
-    c = np.zeros_like(x)
 
-    for i in range(len(x)):
-        a[i] = y[i]
-        h[i] = x[i + 1] - x[i]
+    a = np.copy(y)
+    h = x[1:] - x[:-1]
+    c = np.zeros(x.shape)
+
+    A = np.diag(2 * (h[:-1] + h[1:])) + np.diag(h[1:-1], 1) + np.diag(h[1:-1], -1)
+    z = 3 * (((y[2:] - y[1:-1]) / h[1:-1]) - ((y[1:-1] - y[:-2]) / h[:-2]))
+    c[1:-1] = (np.linalg.solve(A, z))
     
-    for i in range(len(c) - 1):
-        if (i == 1):
-            2 * (h[0] + h[1]) * c[1] + h[1] * c[2] = 
+    b = ((y[1:] - y[:-1]) / h) - (c[1:] + 2 * c[:-1]) * (h/3)
 
-
-    
-
-    b = ((y[1:] - y[:-1]) / h) - (c[1:] + 2 * c[:-1]) * h/3
     d = (c[1:] - c[:-1]) / (h * 3)
 
+    yy = np.zeros(xx.shape)
+    for i in range(x.size - 1):
+        idx = np.nonzero((xx >= x[i]) & (xx <= x[i + 1])) 
+        dx = xx[idx] - x[i]
+        yy[idx] = a[i] + (b[i] * dx) + (c[i] * (dx ** 2)) + (d[i] * (dx ** 3))
+             
+    return yy
 
-spline(x, y, xx)
+print(spline(x, y, xx))
+
