56e96bae898fa980034e37be0935436b18e3fd5a
Joëlle Monhart <65352564+sunnyfisch@users.noreply.github.com>
2024-03-18 16:41:41 +0100

    add aufgabe 2

--- Übungen/JLM/IT22ta_WIN09_S5/IT22ta_WIN09_S5_Aufg2.py
+++ Übungen/JLM/IT22ta_WIN09_S5/IT22ta_WIN09_S5_Aufg2.py
@@ -1,33 +1,66 @@
+import matplotlib.pyplot as plt
 import numpy as np
 
 
 # Lagrange-Interpolation
-def lagrange_int(x, y, x_int):
-    p = 0
-    n = len(x)
+def IT22ta_WIN09_S5_Aufg2(x, y, xx):
+    a = y
+    h = np.arange(x.size - 1)
+    c = np.zeros_like(x)
+    A = np.zeros((x.size - 2, x.size - 2))
+    z = np.zeros(c.size - 2)
+    b = np.zeros(x.size - 1)
+    d = np.zeros(x.size - 1)
+    yy = np.zeros(xx.size)
+    print(yy)
+    for i in range(0, h.size):
+        h[i] = x[i + 1] - x[i]
 
-    for i in range(0, n):
-        l_n = 1
-        for j in range(0, n):
-            if j != i:
-                l_n *= (x_int - x[j]) / (x[i] - x[j])
-        p += y[i] * l_n
-    return p
+    for i in range(0, x.size - 2):
+        A[i, i] = 2 * (h[i] + h[i + 1])
+        if i + 1 < x.size - 2:
+            A[i + 1, i] = h[i + 1]
+            A[i, i + 1] = h[i + 1]
+        z[i] = (3 * ((y[i + 2] - y[i + 1]) / h[i + 1])) - (
+            3 * ((y[i + 1] - y[i]) / h[i])
+        )
+
+    c[1:-1] = np.linalg.solve(A, z)
+
+    for i in range(0, x.size - 1):
+        b[i] = ((y[i + 1] - y[i]) / h[i]) - ((h[i]) / 3 * (c[i + 1] + 2 * c[i]))
+
+    for i in range(0, x.size - 1):
+        d[i] = (1 / (3 * h[i])) * (c[i + 1] - c[i])
+
+    for i in range(xx.size):  # Durchlaufen der Werte in xx
+        for j in range(x.size - 1):  # Durchlaufen der Stützstellen
+            if x[j] <= xx[i] <= x[j + 1]:
+                yy[i] = (
+                    a[j]
+                    + b[j] * (xx[i] - x[j])
+                    + c[j] * ((xx[i] - x[j]) ** 2)
+                    + d[j] * ((xx[i] - x[j]) ** 3)
+                )
+                print(yy[i])
+                break
+    return yy
 
 
 # Gegebene Werte
-x = np.array([0, 2500, 5000, 10000])
-y = np.array([1013, 747, 540, 226])
-x_int = 3750
-y_int = lagrange_int(x, y, x_int)
+x = np.array([4.0, 6.0, 8.0, 10.0])
+y = np.array([6.0, 3.0, 9.0, 0.0])
+xx = np.arange(4.0, 10.1, 0.1)
 
-# Ergebnis
-print(y_int)
+yy = IT22ta_WIN09_S5_Aufg2(x, y, xx)
 
-"""
-from scipy.interpolate import lagrange
-x = np.array([0,2500,5000,10000])
-y = np.array([1013,747,540,226])
-poly = lagrange(x, y)
-print(poly(3750)) 
-"""
+plt.plot(xx, yy)
+plt.plot(x, y, marker="o", linewidth=0)
+# plt.ylim(-100, 250)
+# plt.xlim(1975, 2020)
+plt.xlabel("Jahr")
+plt.ylabel("Haushalte mit Computer [%]")
+plt.title("Aufgabe 3 a - Lagrange")
+plt.legend(["f1(x)"])
+plt.grid()
+plt.show()
