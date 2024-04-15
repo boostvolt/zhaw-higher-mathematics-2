3c92947aecd08e5f384d5b6261f31fc84da6d9ae
Joëlle Monhart <65352564+sunnyfisch@users.noreply.github.com>
2024-04-15 14:01:28 +0200

    add structure

---
+++ Übungen/JLM/IT22ta_WIN09_S9/IT22ta_WIN09_S9_Aufg3.py
@@ -0,0 +1,17 @@
+from math import cos
+
+import numpy as np
+
+
+def IT22ta_WIN09_S9_Aufg3a(f, a, b, m):
+    h[j] = (b - a) / 2**j
+    return T
+
+
+# Erdmasse berechnen
+f = cos(x**2)
+a = 0
+b = np.pi
+j = [0]
+
+[T] = IT22ta_WIN09_S9_Aufg3a(f, a, b, m)
