728da89fffb7aacc43fd62b28076a9d14a8aa4d1
Minh Khanh Nguyen <minhkhanh2010@hotmail.com>
2024-04-28 23:35:38 +0200

    week 10 done
    Co-authored-by: Jan Kott <boostvolt@users.noreply.github.com>
    Co-authored-by: Sascha Huber <saschahuberzh@users.noreply.github.com>

---
+++ Übungen/JMS/Serie10/IT22ta_WIN10_S10_Aufg1a.py
@@ -0,0 +1,45 @@
+import numpy as np
+
+a = 100
+b = 0
+m = 4
+
+f = lambda v: 97000 / ((-5.0) * (v**2) - 570000.0)
+
+
+def sum_trapez(f, a, b, n):
+    h = (b - a) / n
+    res = 0
+    k = (f(a) + f(b)) / 2
+    for i in range(1, n):
+        xi = a + i * h
+        res = res + f(xi)
+    return h * (k + res)
+
+
+def romberg_extrapolation(f, a, b, m):
+    T = np.zeros((m, m))
+
+    for k in range(m):
+        T[k, 0] = sum_trapez(f, a, b, 2**k)
+
+    c = 4
+    for k in range(1, m):
+        T[0 : m - k, k] = (c * T[1 : m - k + 1, k - 1] - T[: m - k, k - 1]) / (c - 1)
+        c *= 4
+
+    print(T)
+
+    return T[0:m]
+
+
+romberg_extrapolation(f, a, b, m)
+
+
+a = 100
+b = 0
+m = 4
+
+f = lambda v: (97000 * v) / ((-5.0) * (v**2) - 570000.0)
+
+romberg_extrapolation(f, a, b, m)
