fd5fc9a5c53560e3431d98216c362a8b716e9509
Joëlle Monhart <65352564+sunnyfisch@users.noreply.github.com>
2024-05-06 00:20:10 +0200

    refactor methods

--- Übungen/JLM/IT22ta_WIN09_S11/IT22ta_WIN09_S11_Aufg1.py
+++ Übungen/JLM/IT22ta_WIN09_S11/IT22ta_WIN09_S11_Aufg1.py
@@ -1,45 +1,43 @@
-import numpy as np
 import matplotlib.pyplot as plt
-
+import numpy as np
 
 
 def richtungsfeld(f, xmin, xmax, ymin, ymax, hx, hy):
     # Erzeuge das Punkteraster in der xy-Ebene
-    x = np.arange(xmin, xmax + hx, hx)
-    y = np.arange(ymin, ymax + hy, hy)
+    x = np.arange(xmin, xmax, hx)
+    y = np.arange(ymin, ymax, hy)
     X, Y = np.meshgrid(x, y)
 
     # Berechne die Steigung für jeden Punkt
-    Ydiff = f(X, Y)
+    # Ydiff = f(X, Y)
 
     # Normalisiere die Längen der Pfeile
-    magnitude = np.sqrt(1 + Ydiff ** 2)
-    U = 1 / magnitude
-    V = Ydiff / magnitude
-
-    # Berechne die Farben basierend auf der Steigung
-    color = np.arctan2(V, U)
+    # magnitude = np.sqrt(1 + Ydiff ** 2)
+    U = np.ones((int((xmax - xmin) / hx), int((ymax - ymin) / hy)))
+    # U = 1 / magnitude
+    V = f(X, Y)
 
     # Plotte das Richtungsfeld
-    plt.figure(figsize=(20, 16))
-    plt.quiver(X, Y, U, V, color, cmap='RdYlGn_r')
-    plt.xlabel('x')
-    plt.ylabel('y')
-    plt.title('Richtungsfeld der Differentialgleichung')
+    plt.figure(figsize=(20, 12))
+    plt.quiver(X, Y, U, V)
+    plt.xlabel("x")
+    plt.ylabel("y")
+    plt.title("Richtungsfeld der Differentialgleichung")
     plt.grid(True)
-    plt.show()
+
 
 if __name__ == "__main__":
-    xmin = 0
-    xmax = 5
-    ymin = 0
+    xmin = -2
+    xmax = 2
+    ymin = -3
     ymax = 3
     hx = 0.1
     hy = 0.1
+
     # Beispiel Funktion f(x, y) = x + y
     def f(x, y):
-        return x**2 + 0.1*y
-
+        return x**2 + 0.1 * y
 
     # Test der Funktion
     richtungsfeld(f, xmin, xmax, ymin, ymax, hx, hy)
+    plt.show()
--- Übungen/JLM/IT22ta_WIN09_S11/IT22ta_WIN09_S11_Aufg3.py
+++ Übungen/JLM/IT22ta_WIN09_S11/IT22ta_WIN09_S11_Aufg3.py
@@ -1,75 +1,50 @@
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
+from IT22ta_WIN09_S11_Aufg1 import richtungsfeld
 
-def f(x, y):
-    return x ** 2 / y
+f = lambda x, y: x**2 / y
+a = 0
+b = 1.4
+n = 2
+y0 = 2
 
 
-def anfangswertproblem(f, a, b, n, y0):
+def IT22ta_WIN10_S11_Aufg3(f, a, b, n, y0):
     h = (b - a) / n
-    x = np.zeros(n + 1)
+    t = np.zeros(n + 1)
     y_euler = np.zeros(n + 1)
     y_mittelpunkt = np.zeros(n + 1)
     y_modeuler = np.zeros(n + 1)
-    x[0] = a
+
+    t[0] = a
     y_euler[0] = y0
     y_mittelpunkt[0] = y0
     y_modeuler[0] = y0
-    for i in range(n):
-        x[i + 1] = x[i] + h
-        y_euler[i + 1] = y_euler[i] + h * f(x[i], y_euler[i])
-        y_mittelpunkt[i + 1] = y_mittelpunkt[i] + h * f(x[i] + h / 2,
-                                                        y_mittelpunkt[i] + h / 2 * f(x[i], y_mittelpunkt[i]))
-        y_modeuler[i + 1] = y_modeuler[i] + h / 2 * (
-                    f(x[i], y_modeuler[i]) + f(x[i + 1], y_modeuler[i] + h * f(x[i], y_modeuler[i])))
-    return [x, y_euler, y_mittelpunkt, y_modeuler]
+
+    for i in range(0, n):
+        t[i + 1] = t[i] + h
+
+        # Euler method
+        y_euler[i + 1] = y_euler[i] + h * f(t[i], y_euler[i])
+
+        # Midpoint method
+        k1 = f(t[i], y_mittelpunkt[i])
+        k2 = f(t[i] + h / 2, y_mittelpunkt[i] + (h / 2) * k1)
+        y_mittelpunkt[i + 1] = y_mittelpunkt[i] + h * k2
+
+        # Modified Euler method
+        k1 = f(t[i], y_modeuler[i])
+        k2 = f(t[i] + h, y_modeuler[i] + h * k1)
+        y_modeuler[i + 1] = y_modeuler[i] + (h / 2) * (k1 + k2)
+
+    return t, y_euler, y_mittelpunkt, y_modeuler
 
 
-# Define the variables
-a = 0
-b = 1.4
-h = 0.7
-# convert to int
-n = int((b - a) / h)
-x = np.zeros(n + 1)
-y = np.zeros(n + 1)
-x[0] = a
-y[0] = 2
-ymin = 2
-ymax = 3
+[x, y_euler, y_mittelpunkt, y_modeuler] = IT22ta_WIN10_S11_Aufg3(f, a, b, n, y0)
+richtungsfeld(f, -2, 2, -0.5, 3, 0.25, 0.25)
+plt.plot(x, y_euler, label="Euler")
+plt.plot(x, y_mittelpunkt, linestyle="--", label="Midpoint")
+plt.plot(x, y_modeuler, linestyle="dotted", label="Modified Euler")
 
-
-
-# Calculate the solution with euler, midpoint and modified euler method
-[x, y_euler, y_mittelpunkt, y_modeuler] = anfangswertproblem(f, a, b, n, y[0])
-
-# Erzeuge das Punkteraster in der xy-Ebene
-x = np.arange(a, b + h, h)
-y_exact = np.sqrt((2 * x ** 3) / 3 + 4)
-
-
-
-X, Y = np.meshgrid(x, y)
-
-# Berechne die Steigung für jeden Punkt
-Ydiff = f(X, Y)
-
-# Normalisiere die Längen der Pfeile
-magnitude = np.sqrt(1 + Ydiff ** 2)
-U = 1 / magnitude
-V = Ydiff / magnitude
-
-# Berechne die Farben basierend auf der Steigung
-color = np.arctan2(V, U)
-
-# Plotte das Richtungsfeld
-plt.figure(figsize=(20, 16))
-plt.plot(x, y_exact)
-plt.quiver(X, Y, U, V, color, cmap='RdYlGn_r')
-plt.xlabel('x')
-plt.ylabel('y')
-plt.title('Richtungsfeld der Differentialgleichung')
-plt.grid(True)
+plt.legend()
 plt.show()
-
-# Plot the exact solution
